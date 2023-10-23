5df706b33847f43eb0398a18da79327015abe6d0
Eric Thompson <thompsoneric3259@gmail.com>
2023-10-23 11:22:37 -0400

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (
+        typeof(num1) !== 'number' ||
+        typeof(num2) !== 'number' ||
+        num1 < 0 || num2 < 0
+    ) {
+        return 'ERROR'
+    } else {
+        const min = (num1 < num2) ? num1 : num2;
+        const max = (num1 > num2) ? num1 : num2;
+        let sum = 0;
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
