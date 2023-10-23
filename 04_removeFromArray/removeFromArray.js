7f8b4ad6d83d91a65a66078811b7dfa9bcfe4aa4
Eric Thompson <thompsoneric3259@gmail.com>
2023-10-23 11:05:46 -0400

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeValues) {
+    for (let i = 0; i < array.length; i++) {
+        for (const value of removeValues) {
+            if (array[i] === value) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
