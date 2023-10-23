5c88dfdf601dd8f64526c4aef22dd30788a06fd1
Eric Thompson <thompsoneric3259@gmail.com>
2023-10-23 11:45:55 -0400

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  const tempCelsius = (tempFahrenheit - 32) / 9 * 5;
+  return +tempCelsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  const tempFahrenheit = tempCelsius * 9 / 5 + 32;
+  return +tempFahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
